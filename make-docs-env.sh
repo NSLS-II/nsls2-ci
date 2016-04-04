e36d85e59b60000bc7b8278ad64aa99815489707
Travis CI <travis@nomail>
2016-04-04 18:28:58 -0400

    CI: Fix bad python version variable

--- make-docs-env.sh
+++ make-docs-env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export CONDA_ENV_NAME=doc-build
-conda create -n $CONDA_ENV_NAME python=$TRAVIS_PYTHON_VERSIONs sphinx numpydoc pip jsonschema ipython matplotlib
+conda create -n $CONDA_ENV_NAME python=$TRAVIS_PYTHON_VERSION sphinx numpydoc pip jsonschema ipython matplotlib
 pip install sphinx_bootstrap_theme sphinxcontrib-napoleon
