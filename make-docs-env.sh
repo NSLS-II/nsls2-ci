a8c07fc0ec4b39bf450cbf047aeeb9e5193a3d61
Travis CI <travis@nomail>
2016-04-04 18:39:13 -0400

    CI: Typo in package to install

--- make-docs-env.sh
+++ make-docs-env.sh
@@ -3,6 +3,6 @@ export CONDA_ENV_NAME=doc-build
 conda create -n $CONDA_ENV_NAME python=$TRAVIS_PYTHON_VERSION sphinx numpydoc pip jsonschema ipython matplotlib
 # activate the environment to install some pip stuff
 source activate $CONDA_ENV_NAME
-pip install sphinx_bootstrap_teme sphinxcontrib-napoleon
+pip install sphinx_bootstrap_theme sphinxcontrib-napoleon
 # deactivate the environment after we're done
 source deactivate
