4700be4116359419505828e93dd5f27d30feb025
Eric Dill <edill@bnl.gov>
2016-05-02 16:25:33 -0400

    Add an else clause explaining why an upload might not take place

--- anaconda-upload.sh
+++ anaconda-upload.sh
@@ -11,4 +11,6 @@ if [[ $ANACONDA_CHANNEL ]]; then
 fi;
 if [[ ( "$TRAVIS_PYTHON_VERSION" != 'nightly' ) && ( "$TRAVIS_PULL_REQUEST" == 'false' ) && ( "$TRAVIS_BRANCH" == $UPLOAD_ONLY_IF_BRANCH_IS ) ]] ; then
   anaconda -t ${BINSTAR_TOKEN} upload $($CONDA_BUILD_COMMAND --output) -u $ANACONDA_USER $ANACONDA_CHANNEL --force;
+else;
+  echo "Not uploading because this is either (1) python nightly, (2) a pull request or (3) not the $UPLOAD_ONLY_IF_BRANCH_IS branch"
 fi;
