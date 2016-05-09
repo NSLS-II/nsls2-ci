3503c27a624cd3a0467fdfeb8ad9ba15972666f3
Eric Dill <edill@bnl.gov>
2016-05-09 16:50:43 -0400

    Default the travis python version to 3.5

--- make-docs-env.sh
+++ make-docs-env.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 export CONDA_ENV_NAME=doc-build
+TRAVIS_PYTHON_VERSION=${TRAVIS_PYTHON_VERSION:=3.5}
 conda create -n $CONDA_ENV_NAME python=$TRAVIS_PYTHON_VERSION sphinx numpydoc pip jsonschema ipython matplotlib
 # activate the environment to install some pip stuff
 source activate $CONDA_ENV_NAME
