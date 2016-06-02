d670c1fe2aac8d1b60876f6dff5dd94205b76953
danielballan <daniel.b.allan@gmail.com>
2016-06-01 21:11:37 -0400

    Allow injection of docs-specific deps for conda and pip.

--- make-docs.sh
+++ make-docs.sh
@@ -10,14 +10,14 @@ message () {
 # export CONDA_ENV_NAME=doc-build
 
 # message "Creating the conda environment for building docs"
-message "Installing dependencies to build the docs"
-conda install sphinx numpydoc pip jsonschema ipython matplotlib
+message "Installing conda dependencies to build the docs"
+conda install -c lightsource2 sphinx numpydoc pip jsonschema ipython matplotlib $DOCS_CONDA_DEPS
 
 # message "activating the sphinx build environment"
 # source activate $CONDA_ENV_NAME
 
 message "Installing pip dependencies for building the docs"
-pip install sphinx_bootstrap_theme sphinxcontrib-napoleon
+pip install sphinx_bootstrap_theme sphinxcontrib-napoleon $DOCS_PIP_DEPS
 
 message "pushd'ing into DOCS_DIR=$DOCS_DIR"
 pushd $DOCS_DIR
