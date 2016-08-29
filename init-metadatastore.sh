998157d80d9560d70dbc932e484c16975e3f875a
arkilic <arkilic@bnl.gov>
2016-08-29 15:09:53 -0400

    Add mds auth params

--- init-metadatastore.sh
+++ init-metadatastore.sh
@@ -2,6 +2,8 @@
 export MDS_HOST=localhost
 export MDS_DATABASE=mds-test
 export MDS_TIMEZONE=US/Eastern
+export MDS_MONGO_USER=tom
+export MDS_MONGO_PWD=jerry
 mkdir -p ~/.config/metadatastore
 ls ~/.config
 ls ~/.config/metadatastore
