5bb0f23cb4d8ad1ebb8b18932fd6a3e55796014c
arkilic <arkilic@bnl.gov>
2016-08-30 15:28:48 -0400

    Add PORT as an env

--- init-metadatastore.sh
+++ init-metadatastore.sh
@@ -4,6 +4,7 @@ export MDS_DATABASE=mds-test
 export MDS_TIMEZONE=US/Eastern
 export MDS_MONGO_USER=tom
 export MDS_MONGO_PWD=jerry
+export MDS_PORT=27017
 mkdir -p ~/.config/metadatastore
 ls ~/.config
 ls ~/.config/metadatastore
