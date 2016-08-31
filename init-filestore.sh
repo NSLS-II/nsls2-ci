992fc9aee3eef5bb903281546db1f6e227caee6b
arkilic <arkilic@bnl.gov>
2016-08-31 10:40:21 -0400

    ENH:Mongo shell authentication params added to FStore-init

--- init-filestore.sh
+++ init-filestore.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 export FS_HOST=localhost
 export FS_DATABASE=fs-test
+export FS_PORT=27017
+export FS_MONGO_USER=tom
+export FS_MONGO_PWD=jerry
 mkdir -p ~/.config/filestore
 ls ~/.config
 ls ~/.config/filestore
 echo "port: 27017" > ~/.config/filestore/connection.yml
+echo "mongo_user: tom" > ~/.config/filestore/connection.yml
+echo "mongo_pwd: jerry" > ~/.config/filestore/connection.yml
+~
